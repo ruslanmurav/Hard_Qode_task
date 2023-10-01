8bcd1ec03e0ac0e83f2e940e50684c6bcfd42f62
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-10-01 14:27:50 +0300

    create Lesson field

--- lesson/models.py
+++ lesson/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=255)
+    link = models.URLField(max_length=200)
+    duration = models.PositiveIntegerField()
+
+
