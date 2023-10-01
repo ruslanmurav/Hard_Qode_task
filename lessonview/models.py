657648c069d2d8a04060f6e3848b0951740571af
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-10-01 14:53:03 +0300

    create Viewing field

--- lessonview/models.py
+++ lessonview/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from lesson.models import Lesson
+
+
+class Viewing(models.Model):
+    lesson = models.ForeignKey(to=Lesson, on_delete=models.CASCADE)
+    duration = models.PositiveIntegerField()
+    viewed = models.BooleanField()
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    
