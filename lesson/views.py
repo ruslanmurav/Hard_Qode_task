2f1586f76a95c833c03c20c79e13ba9fca0277a0
ruslanmurav <ruslanmuraveiko@gmail.com>
2023-10-01 21:13:37 +0300

    create viewset

--- lesson/views.py
+++ lesson/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.viewsets import ModelViewSet
+
+from lesson.models import Lesson
+from lesson.serializers import LessonSerializer
+
+
+class LessonViewSet(ModelViewSet):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
